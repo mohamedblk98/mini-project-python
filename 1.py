d72a3f353725618f35a3b4009a7fe44644652111
mohamedblk98 <128033953+mohamedblk98@users.noreply.github.com>
2023-03-16 10:03:17 +0100

    python mini project

---
+++ 1.py
@@ -0,0 +1,7 @@
+email = input("Enter your email address: ")
+username = email.split("@")[0]
+domain_name = email.split("@")[1]
+
+print("Username:", username)
+print("Domain Name:", domain_name)
+
---
+++ 3.py
@@ -0,0 +1,6 @@
+import random
+dice1=random.choice([1,2,3,4,5,6])
+dice2=random.choice([1,2,3,4,5,6])
+print(dice1)
+print('..................................')
+print(dice2)
